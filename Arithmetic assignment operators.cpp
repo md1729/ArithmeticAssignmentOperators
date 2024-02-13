2353baa6f2851bae7e58e19d378f4f44cdea0e53
md1729 <muhammet.dilek@metu.edu.tr>
2024-02-13 23:32:20 +0300

    Arithmetic Assignment Operators with Cpp
    Arithmetic Assignment Operators with CPP programming language for this project

---
+++ Arithmetic assignment operators.cpp
@@ -0,0 +1,26 @@
+/*Arithmetic assignment operators
++=by adding
+-=by subtracting
+*=by multiplying
+/=by dividing
+
+*/
+
+#include<iostream>
+using namespace std;
+main(){
+int a= 20;
+int b= 10;
+/*
+a=a-5;//a-=5
+a=a+5;//a+=5
+*/
+a/=b;//a=a/b;
+
+cout<<a;
+
+
+
+	
+	
+}
